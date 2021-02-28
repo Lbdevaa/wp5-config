5a6609bc1809544d2c57327c5b34142e19545611
Merak <mesheryakov97@gmail.com>
2021-02-28 21:53:24 +0500

    $add:  react refresh for dev

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
+const plugins = [];
+
+if (process.env.NODE_ENV !== 'production'){
+  plugins.push('react-refresh/babel');
+}
+
 module.exports = {
   presets: [
     '@babel/preset-env', 
@@ -6,5 +12,7 @@ module.exports = {
         runtime: 'automatic'
       }
     ],
-  ]
+  ],
+  plugins: plugins,
+
 }
