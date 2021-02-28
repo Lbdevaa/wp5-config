ede0bf766c15ce47018f8ed07e56af7ec0ac5e88
Merak <mesheryakov97@gmail.com>
2021-02-28 16:09:40 +0500

    $add: webpack clean and html plugins

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 //package.json conf for any "build": "set NODE_ENV=production&&webpack",
 // no &&
 // no set
+// rd /s /q
+// "clean": "rm -rf dist"
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
-const  MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 let mode = 'development';
 let target = 'web'; //fix for live reload
 
@@ -12,6 +16,7 @@ module.exports = {
   target: target,
 
   output: {
+    path: path.resolve(__dirname, 'dist'),
     assetModuleFilename: 'img/[hash][ext][query]'
   },
   module: {
@@ -53,7 +58,11 @@ module.exports = {
   },
 
   plugins: [
-    new MiniCssExtractPlugin()
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
   ],
 
   resolve: {
